928e4b8d95515c264a91a953444e4d85cd9eadaf
Vladimir_Sh <user@example.com>
2019-08-24 06:59:32 -0400

    implemented mocker model and tests

--- mocker/models.py
+++ mocker/models.py
@@ -1,3 +1,97 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.utils import timezone
+from django.db import models, transaction
+from django.db.models import Q, QuerySet, Model
+from django.contrib.postgres.fields import JSONField
+
+
+TTL = 120
+ANY = '*'
+CYCLE = 'cycle'
+SEQUENCE = 'sequence'
+
+
+class DupError(ValueError):
+    pass
+
+
+class Lock(Model):
+    name = models.CharField(max_length=16, primary_key=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class MockQuerySet(QuerySet):
+
+    def overlapped(self, host, route, method):
+        qs = self.filter(expired__gt=timezone.now())
+
+        if host != ANY:
+            qs = qs.filter(host__in=(host, ANY))
+
+        if route != ANY:
+            qs = qs.filter(route__in=(route, ANY))
+
+        if method != ANY:
+            qs = qs.filter(method__in=(method, ANY))
+
+        return qs
+
+    def not_by_response(self, responses, response_type):
+        return self.filter(~Q(response_type=response_type, responses=responses) | Q(is_exclusive=True))
+
+    def by_response(self, responses, response_type):
+        return self.filter(response_type=response_type, responses=responses)
+
+    @transaction.atomic
+    def create(self, route, method, responses, name=ANY, ttl=TTL, host=ANY, response_type=SEQUENCE, is_exclusive=False):  # TODO: hardcode
+        Lock.objects.select_for_update().get_or_create(name='Mock')
+
+        overlapped = self.overlapped(host, route, method).not_by_response(responses, response_type)
+        if overlapped.exists():
+            raise DupError('Can\'t to create mock, '
+                           f'try again later, please (overlapped {list(overlapped[:10])})')
+
+        overlapped = self.overlapped(host, route, method).by_response(responses, response_type)
+        if overlapped.exists() and is_exclusive:
+            raise DupError('Can\'t to create exclusive mock, '
+                           f'try again later, please (overlapped {list(overlapped[:10])})')
+
+        # noinspection PyArgumentList
+        mock = super().create(name=name, ttl=ttl, host=host, route=route, method=method,
+                              responses=responses, response_type=response_type, is_exclusive=is_exclusive)
+
+        return mock
+
+
+class Mock(Model):
+    name = models.CharField(max_length=128, blank=True, default=ANY)
+    created = models.DateTimeField(auto_now_add=True)
+    expired = models.DateTimeField(null=True, blank=True)
+    ttl = models.PositiveIntegerField(default=TTL)
+    is_exclusive = models.BooleanField(default=False)
+    # --
+    host = models.CharField(max_length=120, default=ANY)
+    route = models.CharField(max_length=120, default=ANY)
+    method = models.CharField(max_length=120, default=ANY)
+    # --
+    responses = JSONField()
+    response_type = models.CharField(max_length=20,
+                                     choices=((CYCLE, CYCLE), (SEQUENCE, SEQUENCE)),
+                                     default=SEQUENCE)
+
+    objects = MockQuerySet.as_manager()
+
+    def save(self, *args, **kwargs):
+        if not self.expired:
+            self.expired = timezone.now() + datetime.timedelta(seconds=self.ttl)
+        # noinspection PyArgumentList
+        super().save(*args, **kwargs)
+
+    def is_expired(self):
+        return self.expired < timezone.now()
+
+    def __str__(self):
+        return f'{self.name}, id: {self.pk}, expired: {self.expired}'
--- mocker/tests.py
+++ mocker/tests.py
@@ -1,12 +1,121 @@
 import pytest
+from mocker.models import Mock, DupError, ANY, CYCLE, SEQUENCE
+
 
 pytestmark = pytest.mark.django_db
 
 
-def test_create_first_mock():
-    assert False
+def test_create_unique_mock():
+    Mock.objects.create(host='some.any', route='/', method='GET', responses='resp 1')
+    Mock.objects.create(host='www.some.any', route='/', method='GET', responses='resp 2')
+    Mock.objects.create(host='www.some.any', route='/1', method='GET', responses='resp 3')
+    Mock.objects.create(host='www.some.any', route='/1', method='POST', responses='resp 4')
+    Mock.objects.create(host=ANY, route='/1', method='PUT', responses='resp 5')
+    Mock.objects.create(host=ANY, route=ANY, method='HEAD', responses=['resp 6'])
 
 
-class TestMockModel:
-    def test_create_first_mock(self):
-        assert False
+@pytest.mark.parametrize(
+    'first,second', [
+        (
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp'},
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp'},
+        ),
+        (
+            {'host': ANY, 'route': ANY, 'method': 'GET', 'responses': 'resp'},
+            {'host': ANY, 'route': '/', 'method': ANY, 'responses': 'resp'},
+        ),
+        (
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp'},
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp'},
+        ),
+    ]
+)
+def test_create_similar_mock(first, second):
+    Mock.objects.create(**first)
+    Mock.objects.create(**second)
+
+
+@pytest.mark.parametrize(
+    'first,dup', [
+        pytest.param(
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1'},
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1 for dup'},
+            id='dup_by_all'
+        ),
+        pytest.param(
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1'},
+            {'host': ANY, 'route': '/', 'method': 'GET', 'responses': 'resp 1 for dup'},
+            id='dup_by_host_any_second'
+        ),
+        pytest.param(
+            {'host': ANY, 'route': '/', 'method': 'GET', 'responses': 'resp 1'},
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1 for dup'},
+            id='dup_by_host_any_first'
+        ),
+        pytest.param(
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1'},
+            {'host': 'some.any', 'route': ANY, 'method': 'GET', 'responses': 'resp 1 for dup'},
+            id='dup_by_any_route_second'
+        ),
+        pytest.param(
+            {'host': 'some.any', 'route': ANY, 'method': 'GET', 'responses': 'resp 1'},
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1 for dup'},
+            id='dup_by_any_route_first'
+        ),
+        pytest.param(
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1'},
+            {'host': 'some.any', 'route': '/', 'method': ANY, 'responses': 'resp 1 for dup'},
+            id='dup_by_any_method_second'
+        ),
+        pytest.param(
+            {'host': 'some.any', 'route': '/', 'method': ANY, 'responses': 'resp 1'},
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1 for dup'},
+            id='dup_by_any_method_first'
+        ),
+        pytest.param(
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1'},
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp 1 for dup'},
+            id='dup_by_any_all_second'
+        ),
+        pytest.param(
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp 1'},
+            {'host': 'some.any', 'route': '/', 'method': 'GET', 'responses': 'resp 1 for dup'},
+            id='dup_by_any_all_first'
+        ),
+        pytest.param(
+            {'route': '/', 'method': 'GET', 'responses': ['resp 1', 'resp 2'], 'response_type': SEQUENCE},
+            {'route': '/', 'method': 'GET', 'responses': ['resp 1', 'resp 2'], 'response_type': CYCLE},
+            id='dup_by_response_type'
+        ),
+        pytest.param(
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp 1', 'is_exclusive': True},
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp 1'},
+            id='dup_by_exclusive_first'
+        ),
+        pytest.param(
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp 1'},
+            {'host': ANY, 'route': ANY, 'method': ANY, 'responses': 'resp 1', 'is_exclusive': True},
+            id='dup_by_exclusive_second'
+        ),
+    ]
+)
+def test_create_overlapped_mock(first, dup):
+    Mock.objects.create(**first)
+
+    with pytest.raises(DupError):
+        Mock.objects.create(**dup)
+
+
+def test_expired_mock():
+    m = Mock.objects.create(route=ANY, method=ANY, responses='resp 1', ttl=0)
+    assert m.is_expired()
+
+
+def test_not_expired_mock():
+    m = Mock.objects.create(route=ANY, method=ANY, responses='resp 1', ttl=60)
+    assert not m.is_expired()
+
+
+def test_create_dup_over_expired_mock():
+    Mock.objects.create(route=ANY, method=ANY, responses='resp 1', ttl=0)
+    Mock.objects.create(route=ANY, method=ANY, responses='resp 2')
