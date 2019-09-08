b095ba4400b4953c5ad8af5640f153b07fd0c594
Vladimir_Sh <user@example.com>
2019-09-08 08:47:59 -0400

    adopts for travisci

--- code/scripts/entrypoint.sh
+++ code/scripts/entrypoint.sh
@@ -2,8 +2,16 @@
 
 echo "waiting for DB "
 
+
+attempt=1
 while ! python manage.py migrate --noinput
 do
+  if [ $attempt -le 15 ]
+  then
+    break
+  fi
+  attempt=$(( $attempt + 1 ))
+
   echo -n .
   sleep 1
 done
