ab34b0b623cff25fa803aa78db273ae7fe0d8595
Vladimir_Sh <user@example.com>
2019-10-17 05:49:15 -0400

    [api] added ability bulk creation of mocks

--- code/api/tests/tests_api_create_mock.py
+++ code/api/tests/tests_api_create_mock.py
@@ -2,7 +2,7 @@ import pytest
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
-from mocker.models import Mock, CYCLE
+from mocker.models import Mock, CYCLE, SINGLE
 
 
 pytestmark = pytest.mark.django_db
@@ -61,3 +61,73 @@ def test_api_delete_mock(api_client, ttl, expired):
     assert resp.status_code == 200
     assert Mock.objects.filter(name='for_delete').first() is None
     assert resp.json()['is_expired'] is expired
+
+
+def test_api_create_mocks(api_client):
+    resp = api_client.post(
+        reverse('mock-list'),
+        [
+            {
+                'name': 'from_test_42_1', 'route': '/42_1', 'method': 'GET', 'response_type': CYCLE,
+                'responses': [42, 'Hi', {'body': 'secret', 'return_code': 201}]
+            },
+            {
+                'name': 'from_test_42_2', 'route': '/42_2', 'method': 'GET', 'responses': 42,
+            },
+        ],
+        format='json'
+    )
+    assert resp.status_code == 201
+
+    mock = Mock.objects.get(name='from_test_42_1')
+    assert mock.route == '/42_1'
+    assert mock.method == 'GET'
+    assert mock.response_type == CYCLE
+    assert mock.responses == [42, 'Hi', {'body': 'secret', 'return_code': 201}]
+
+    mock = Mock.objects.get(name='from_test_42_2')
+    assert mock.route == '/42_2'
+    assert mock.method == 'GET'
+    assert mock.response_type == SINGLE
+    assert mock.responses == 42
+
+
+def test_api_rollbacks_create_overlapped_mocks(api_client):
+    Mock.objects.create(name='for_rollback', route='/', method='GET', responses='first')
+    resp = api_client.post(
+        reverse('mock-list'),
+        [
+            {
+                'name': 'from_test_42_1', 'route': '/42_1', 'method': 'GET', 'response_type': CYCLE,
+                'responses': [42, 'Hi', {'body': 'secret', 'return_code': 201}]
+            },
+            {
+                'name': 'from_test_42_2', 'route': '/', 'method': 'GET', 'responses': 42,  # dup by route
+            },
+        ],
+        format='json'
+    )
+    assert resp.status_code == 409
+
+    assert Mock.objects.filter(name='from_test_42_1').first() is None
+    assert Mock.objects.filter(name='from_test_42_2').first() is None
+
+
+def test_api_create_mocks_negative(api_client):
+    resp = api_client.post(
+        reverse('mock-list'),
+        [
+            {
+                'name': 'from_test_42_1', 'route': '/42_1', 'method': 'GET', 'response_type': CYCLE,
+                'responses': [42, 'Hi', {'body': 'secret', 'return_code': 201}]
+            },
+            {
+                'name': 'from_test_42_2', 'method': 'GET', 'responses': 42,  # missed 'route' -> bad request
+            },
+        ],
+        format='json'
+    )
+    assert resp.status_code == 400
+
+    assert Mock.objects.filter(name='from_test_42_1').first() is None
+    assert Mock.objects.filter(name='from_test_42_2').first() is None
--- code/api/views.py
+++ code/api/views.py
@@ -1,8 +1,9 @@
+from django.db import transaction
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from api.serializers import MockSerializer
-from mocker.models import Mock, DupError
+from mocker.models import Lock, Mock, DupError
 
 
 class MockViewSet(viewsets.ModelViewSet):
@@ -10,8 +11,26 @@ class MockViewSet(viewsets.ModelViewSet):
     serializer_class = MockSerializer
 
     def create(self, request, *args, **kwargs):
+        if isinstance(request.data, dict):
+            return self._create_one(request, *args, **kwargs)
+        return self._create_list(request, *args, **kwargs)
+
+    def _create_one(self, request, *args, **kwargs):
         try:
             return super().create(request, *args, **kwargs)
+        except DupError as e:
+            return Response({'Fail': str(e)}, status=status.HTTP_409_CONFLICT)
+
+    def _create_list(self, request, *_, **__):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            with transaction.atomic():
+                Lock.objects.select_for_update().get_or_create(name='Mocks')  # for avoid livelock
+                self.perform_create(serializer)
+                headers = self.get_success_headers(serializer.data)
+                return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
         except DupError as e:
             return Response({'Fail': str(e)}, status=status.HTTP_409_CONFLICT)
 
